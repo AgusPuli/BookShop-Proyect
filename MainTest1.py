169b180f93a24145184364b675fe86578582a2ea
AgusPuli <agustinmpulido@gmail.com>
2024-09-02 18:05:12 -0300

    Upload of first WorkingCode

--- MainTest1.py
+++ MainTest1.py
@@ -8,29 +8,32 @@ print("Conexión establecida.")
 
 
 #def space
-def EliminarTotal():        #delet libros info
+
+#   F_Elimina la tabla
+def EliminarTotal():
     cursor.execute('''
     delete from Base
     ''')
     connection.commit()
 
-
-def creartabla():               #crea la tabla primaria
+#   F_Crea la tabla
+def creartabla():
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Base (
         id INTEGER PRIMARY KEY,
         articulo TEXT,
-        stock INTEGER,
-        precio FLOAT
+        precio FLOAT,
+        stock INTEGER
+        
     )
     ''')
-    print("Tabla creada.")
 
+    print("Tabla creada.")
     connection.commit()
 
 
 
-
+#   F_Agregar un articulo
 def AgregarPersonalizado(articulo,stock,precio):
     cursor.execute('''
     insert into Base(articulo, stock, precio)
@@ -42,41 +45,99 @@ def AgregarPersonalizado(articulo,stock,precio):
 
 
 
+# F_Mostrar     //Sin Uso por ahora
 
-def PrintTablas():
-    cursor.execute('SELECT * FROM libros')
-    libros = cursor.fetchall()
 
-    for libroff in libros:
-        print(libroff)
 
-def EliminarTitulo():
+#   F_Elimina un elemento
+def EliminarArticulo(articulo):
     if False:
         cursor.execute('''
-        DELETE FROM libros
-        WHERE titulo = 'Cien Años de Soledad'
-        ''')
+        DELETE FROM Base
+        WHERE articulo = ?
+        ''',(articulo,))
 
         connection.commit()
+        print("Articulo eliminado.")
 
+
+#   F_Aumentar porecio por oporcentaje
 def AumentarPrecio(articuloaux, num):
     cursor.execute('''
-    update Base
-    set precio= precio+precio*(?/100)
+    select precio from Base
     where articulo=?
-    ''',(num,articuloaux))
+    ''',(articuloaux,))
+    precio = (cursor.fetchone())[0]
+    precio=precio+precio*(num/100)
+
+    cursor.execute('''
+    update Base
+    set precio= ?
+    where articulo=?
+    ''',(precio, articuloaux))
     connection.commit()
 #creartabla()
 #Agregarbasic()
 #AgregarLista()
 #AgregarPersonalizado("aladin","hernan",3000,"puli")
 #EliminarTotal()
-print("Enter:1 to create table ")
-print("Enter:2 to add articulo ")
-print("Enter:3 to delete all info from Base ")
-print("Enter:any else to quit")
 
-while True:
+def Menu():
+    while True:
+        print()
+        print()
+        print("Ingrese 1 para crear una nueva tabla \nIngrese 2 para añadir un articulo o aumentar su precio\nIngrese 3 para eliminar un articulo o disminuir su precio")
+        election = int(input("ingrese el numero: "))
+        if election == 1:
+            creartabla()
+
+        elif election == 2:
+            election = int(input("Ingrese 1 para añadir un articulo o 2 para aumentar su precio"))
+
+            if election == 1:
+                articuloaux, stockaux, precioaux = (str(input("Ingrese el articulo: ")),
+                int(input("Ingrese el stock: ")), float(input("Ingrese el precio: ")))
+                AgregarPersonalizado(articuloaux, stockaux, precioaux)
+
+            elif election == 2:
+                articulo, aumento = str(input("Ingrese el articulo: ")), int(
+                input("Ingrese el porcentaje de aumento sin el simbolo -%: "))
+                AumentarPrecio(articulo, aumento)
+
+
+        elif election == 3:
+            election = int(input("Ingrese 1 para eliminar un articulo o 2 para eliminar todo el contenido de la tabla"))
+            if election == 1:
+                EliminarArticulo(str(input("ingrese el articulo a eliminar")))
+
+            elif election == 2:
+                EliminarTotal()
+
+        else:
+            break
+
+
+
+
+
+
+
+"""while True:
+
+    print()
+    print()
+    print("Enter:1 to create table ")
+    print("Enter:2 to add articulo ")
+    print("Enter:3 to delete all info from Base ")
+    print("Enter:4 to increase price ")
+    print("Enter:5 to  delete an article")
+    print("Enter:any other key to exit")
+
+
+
+
+
+
     election=int(input("ingrese el numero: "))
     if election == 1:
         creartabla()
@@ -87,13 +148,16 @@ while True:
     elif election == 3:
         EliminarTotal()
     elif election == 4:
-        articulo=str(input("Ingrese el articulo: "))
-        AumentarPrecio(articulo,100)
+        articulo, aumento=str(input("Ingrese el articulo: ")),int (input("Ingrese el porcentaje de aumento sin el simbolo -%: "))
+        AumentarPrecio(articulo,aumento)
+    elif election == 5:
+        Articulo=str(input("Ingrese el articulo: "))
+        EliminarArticulo(Articulo)
     else:
-        break
+        break"""
 
 
-
+Menu()
 
 
 connection.close()
